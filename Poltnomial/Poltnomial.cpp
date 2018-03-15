07c5a7242014d9cd7d19d4c0f7bf496311356f48
zxyxqx <zxyxqx@126.com>
2018-03-15 18:19:58 +0800

    2

---
+++ Poltnomial/Poltnomial.cpp
@@ -0,0 +1,71 @@
+#include "Poltnomial.h"
+using namespace std;
+Polynomial Polynomial::Add(Polynomial a,Polynomial b)
+{
+    Polynomial c;
+    int aPos=0;
+    int bPos=0;
+    while((aPos<a.terms)&&(bPos<b.terms))                       //a,b两个任意多项式没有搜到数组尾的时候
+    {
+        if((a.termArray[aPos].exp)==(b.termArray[bPos].exp))    //如果指数相同，系数相加
+        {
+            float t;
+            t=a.termArray[aPos].coef+b.termArray[bPos].coef;
+            if(t){ c.NewTerm(c,t,c.termArray[aPos].exp);}                //相加后归到c数组
+            aPos++;             
+            bPos++;
+        }
+        else if(a.termArray[aPos].exp<b.termArray[bPos].exp)             //因为多项式从大到小，所以B大的B先归
+        {
+            c.NewTerm(b,b.termArray[bPos].coef,b.termArray[bPos].exp);
+            bPos++;
+        }
+        else                                                             //因为多项式从大到小，所以A大的A先归        
+        {
+            c.NewTerm(a,a.termArray[aPos].coef,a.termArray[aPos].exp);
+            aPos++;
+        }
+        for(;aPos<=a.terms;aPos++)                                       //任意一个多项式处理完则直接把另一个多项式加到队尾
+        {
+            c.NewTerm(a,a.termArray[aPos].coef,a.termArray[aPos].exp);
+        }
+        for(;bPos<=b.terms;bPos++)
+        {
+            c.NewTerm(b,b.termArray[bPos].coef,b.termArray[bPos].exp);
+        }
+    }
+}
+
+void Poltnomial::NewTerm(Polynomial a,const float theCoeff,const int theExp)
+//Add a new term to the end of termArray  添加一个新的项在多项式的数组尾
+{
+   if(a.terms == a.capacity)
+   {
+       a.capacity*=2;
+       Term *temp=new term[a.capacity];             //根据重新乘好的参数  重新设置一个 new array
+       copy(a.termArray,a.termArray+a.terms,temp);
+       delete [] a.termArray;                       //删除掉以前的数组。
+       a.termArray=temp;
+   }
+   a.terms=a.terms+1;
+   a.termArray[terms].coef=theCoeff;
+   a.termArray[terms].exp=theExp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//test
+
+
+
---
+++ Poltnomial/Poltnomial.h
@@ -0,0 +1,28 @@
+//Polymial ADT
+class Polynomial    //forward declaration
+{
+//p(x)=a0X^e0+a1X^e1+...+anx^ex  是二元组<ei,ai>的有序集合 x是变量
+//其中，ai是非零的浮点系数值，ei是非负的整数型指数值。    
+public:
+    Polynomial();
+    //构造一个空的多项式p(x)=0;
+    Polynomial Add(Polynomial poly);
+    //返回来个多项式的和 *this+poly
+    Polynomial Mult(Polynomial poly);
+    //返回两个多项式*this和poly的乘积
+    Polynomial NewTerm(const float theCoeff,const int theExp);
+    //Add a new term to the end of termArray  添加一个新的项在多项式的后面
+    float Eval(float f);
+    //计算多项式 *this 的变量取f的值，并且返回结果。    
+private:
+    Term *termArray; //  array of nonzero terms;     多项式非零项数组；
+    int capacity;    //  size of termArray;          多项式数组的大小  
+    int terms;       //  number of nonzero terms;    项数的多少；
+};
+class Term      
+{
+    friend Polynomial; //当一个类作为另一个类的友元时，这就意味着这个类的所有成员函数都是另一个类的友元函数，都可以访问另一个类中的隐藏信息（包括私有成员和保护成员）。
+    private:    
+    float coef;  //变量 
+    int exp;     //指数
+}
